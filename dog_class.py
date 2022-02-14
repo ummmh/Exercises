69299c2ed8c96d3c719c0e0a1e8e44bb7e74fe9e
ummmh <eugenioj2@middleton.school.nz>
2022-02-15 12:31:40 +1300

    Update dog_class.py

--- dog_class.py
+++ dog_class.py
@@ -9,10 +9,20 @@ class Dog:
     def print_details(self):
         return f"{self.name} is a {self.colour} dog aged {self.age}."
 
+    def change_age(self, age):
+        self.age = age
+
 
 # Main routine
 dog1 = Dog("Spot", 7, "black")
 dog2 = Dog("Jazz", 5, "white")
+
+print(Dog.print_details(dog1))
+print(Dog.print_details(dog2))
+
+# Change the age function called
+dog1.change_age(17)
+dog2.change_age(9)
 
 print(Dog.print_details(dog1))
 print(Dog.print_details(dog2))
