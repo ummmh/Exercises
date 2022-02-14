bbdf35ebcce835645a68358fa251d3cf29accaab
ummmh <eugenioj2@middleton.school.nz>
2022-02-15 12:28:42 +1300

    Update dog_class.py

--- dog_class.py
+++ dog_class.py
@@ -1,11 +1,18 @@
 # program order: libraries, classes, function, main routine
 # Class - first letter uppercase (camelcase)
 class Dog:
-    def __init__(self, name):
+    def __init__(self, name, age, colour):
         self.name = name
-        print(name)
+        self.age = age
+        self.colour = colour
+
+    def print_details(self):
+        return f"{self.name} is a {self.colour} dog aged {self.age}."
 
 
 # Main routine
-dog1 = Dog("Spot")
-dpg2 = Dog("Jazz")
+dog1 = Dog("Spot", 7, "black")
+dog2 = Dog("Jazz", 5, "white")
+
+print(Dog.print_details(dog1))
+print(Dog.print_details(dog2))
