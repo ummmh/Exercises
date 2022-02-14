34592f31f2bfd94a9c2ed128da77e86e471e5a03
ummmh <eugenioj2@middleton.school.nz>
2022-02-15 12:13:58 +1300

    Create dog_class.py

---
+++ dog_class.py
@@ -0,0 +1,10 @@
+# program order: libraries, classes, function, main routine
+# Class - first letter uppercase (camelcase)
+class Dog:
+    def bark(self):
+        print("Bark")
+
+
+# Main routine
+spot = Dog()
+print(type(spot))
