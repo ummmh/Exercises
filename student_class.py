80aa0ade62941af56286117cfcdb917b7db22c74
ummmh <eugenioj2@middleton.school.nz>
2022-02-15 12:41:58 +1300

    Create student_class.py

---
+++ student_class.py
@@ -0,0 +1,34 @@
+# program order: libraries, classes, function, main routine
+class Student:
+    def __init__(self, name, age, grade):
+        self.name = name
+        self.age = age
+        self.grade = grade # int 0-100
+
+# Class - first letter uppercase (camelcase)
+class Dog:
+    def __init__(self, name, age, colour):
+        self.name = name
+        self.age = age
+        self.colour = colour
+
+    def print_details(self):
+        return f"{self.name} is a {self.colour} dog aged {self.age}."
+
+    def change_age(self, age):
+        self.age = age
+
+
+# Main routine
+dog1 = Dog("Spot", 7, "black")
+dog2 = Dog("Jazz", 5, "white")
+
+print(Dog.print_details(dog1))
+print(Dog.print_details(dog2))
+
+# Change the age function called
+dog1.change_age(17)
+dog2.change_age(9)
+
+print(Dog.print_details(dog1))
+print(Dog.print_details(dog2))
