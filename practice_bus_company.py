e5989126a713425351c0912676861657de509d0b
ummmh <eugenioj2@middleton.school.nz>
2022-02-18 13:47:33 +1300

    Update practice_bus_company.py

--- practice_bus_company.py
+++ practice_bus_company.py
@@ -2,24 +2,25 @@
 # 17/02/2022
 
 class Bus:
+    bus_list = []
+
     def __init__(self, number, route, driver):
         self.number = number
         self.route = route
         self.driver = driver
-        bus_list.append(self)
+        Bus.bus_list.append(self)
 
     def print_details(self):
-        return f"Bus no.: {self.number}\nBus route: {self.route}" \
-               f"\nBus driver: {self.driver}\n"
+        for bus in Bus.bus_list:
+            if bus == self:
+                print(f"Bus no.: {bus.number}\nBus route: {bus.route}"
+                      f"\nBus driver: {bus.driver}\n")
 
 
 # Main Routine
-bus_list = []
-
 bus1 = Bus(2010, "Y", "Greg")
 bus2 = Bus(2011, "P", "Joel")
 bus3 = Bus(2012, "130", "Kent")
 
-for bus in bus_list:
-    print(f"Bus no.: {bus.number}\nBus route: {bus.route}"
-          f"\nBus driver: {bus.driver}\n")
+for bus in range(len(Bus.bus_list)):
+    Bus.print_details(Bus.bus_list[bus])
