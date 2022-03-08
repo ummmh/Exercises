384d12dbb71bb08c81c55af1e720f260918db375
ummmh <eugenioj2@middleton.school.nz>
2022-03-08 13:37:05 +1300

    Update ex2_gui_image.py

--- 04 GUI intro/ex2_gui_image.py
+++ 04 GUI intro/ex2_gui_image.py
@@ -8,11 +8,11 @@ root.title("Using a label to place an image")
 cat_drawing = PhotoImage(file="wonky cat.png")
 
 # placing the image into the label
-image_label = Label(root, image=cat_drawing)
+image_label = Label(root, image=cat_drawing, bg="white")
 image_label.pack()
 
 # adding text below the label
-text_label = Label(root, text="cat", font=("Comic Sans MS", 25))
-text_label.pack()
+text_label = Label(root, text="cat", font=("Comic Sans MS", 25), bg="white")
+text_label.pack(fill=X)
 
 root.mainloop()
