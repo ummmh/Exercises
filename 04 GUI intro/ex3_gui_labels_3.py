43c0100be4d4843740e906fd393b64ad9151a94d
ummmh <eugenioj2@middleton.school.nz>
2022-03-08 13:37:41 +1300

    Update ex3_gui_labels_3.py

--- 04 GUI intro/ex3_gui_labels_3.py
+++ 04 GUI intro/ex3_gui_labels_3.py
@@ -5,14 +5,14 @@ root = Tk()
 root.title("Exercise 1")
 computer = Label(root, bg="lime", fg="white", text="computer",
                  font=("Times", 50, "italic"))
-computer.pack()
+computer.pack(fill=X)
 
 science = Label(root, bg="blue", fg="yellow", text="science is",
                 font=("Comic Sans MS", 50, "bold"))
-science.pack()
+science.pack(fill=X)
 
 awesome = Label(root, bg="orange", fg="red", text="awesome!",
                 font=("Arial Black", 50))
-awesome.pack()
+awesome.pack(fill=X)
 
 root.mainloop()
