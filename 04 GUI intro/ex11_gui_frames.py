751a21aaa9c1d64c3f34bec2224639a1c6809602
ummmh <eugenioj2@middleton.school.nz>
2022-03-09 11:22:23 +1300

    Create ex11_gui_frames.py

---
+++ 04 GUI intro/ex11_gui_frames.py
@@ -0,0 +1,36 @@
+from tkinter import *
+
+root = Tk()
+root.title("My first button")
+
+top_frame = Frame(root)
+top_frame.pack()
+
+bottom_frame = Frame(root)
+bottom_frame.pack()
+
+happy_label = Label(top_frame, text="Happy")
+happy_label.pack()
+
+dog_label = Label(top_frame, text="Dog")
+dog_label.pack()
+
+happy_button = Button(top_frame, text="Happy")
+happy_button.pack(side=LEFT)
+
+sad_button = Button(top_frame, text="Sad")
+sad_button.pack(side=LEFT)
+
+creepy_button = Button(top_frame, text="Creepy")
+creepy_button.pack(side=LEFT)
+
+dog_button = Button(bottom_frame, text="Dog")
+dog_button.pack(side=LEFT)
+
+clown_button = Button(bottom_frame, text="Clown")
+clown_button.pack(side=LEFT)
+
+banana_button = Button(bottom_frame, text="Banana")
+banana_button.pack(side=LEFT)
+
+root.mainloop()
