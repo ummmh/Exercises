8a20e5017bd1b229f1b43683aca93664abab6e96
ummmh <eugenioj2@middleton.school.nz>
2022-03-09 12:04:59 +1300

    Update ex10_gui_frames.py

--- 04 GUI intro/ex10_gui_frames.py
+++ 04 GUI intro/ex10_gui_frames.py
@@ -6,14 +6,17 @@ root.title("Exercise 10")
 top_frame = Frame(root)
 top_frame.pack()
 
+frame = Frame(top_frame)
+frame.pack(side=TOP)
+
 bottom_frame = Frame(root)
 bottom_frame.pack()
 
-happy_label = Label(top_frame, text="Happy")
-happy_label.pack()
+happy_label = Label(frame, text="Happy")
+happy_label.pack(side=LEFT)
 
-dog_label = Label(top_frame, text="Dog")
-dog_label.pack()
+dog_label = Label(frame, text="Dog")
+dog_label.pack(side=LEFT)
 
 happy_button = Button(top_frame, text="Happy")
 happy_button.pack(side=LEFT)
