4c0c3f48cdefa9b7292c7a23f16a689e2fcbaf72
ummmh <eugenioj2@middleton.school.nz>
2022-03-08 13:40:12 +1300

    Create ex4_gui_labels_4.py

---
+++ 04 GUI intro/ex4_gui_labels_4.py
@@ -0,0 +1,18 @@
+from tkinter import *
+
+root = Tk()
+
+root.title("Exercise 1")
+computer = Label(root, bg="lime", fg="white", text="computer",
+                 font=("Times", 50, "italic"))
+computer.pack(fill=X, side=LEFT)
+
+science = Label(root, bg="blue", fg="yellow", text="science is",
+                font=("Comic Sans MS", 50, "bold"))
+science.pack(fill=X, side=RIGHT)
+
+awesome = Label(root, bg="orange", fg="red", text="awesome!",
+                font=("Arial Black", 50))
+awesome.pack(fill=X, side=RIGHT)
+
+root.mainloop()
