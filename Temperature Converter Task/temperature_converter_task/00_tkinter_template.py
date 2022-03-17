ecd0785e8544cae1aee4b4ae7df14138420c427e
ummmh <eugenioj2@middleton.school.nz>
2022-03-17 13:41:59 +1300

    Create 00_tkinter_template.py

---
+++ Temperature Converter Task/temperature_converter_task/00_tkinter_template.py
@@ -0,0 +1,15 @@
+from tkinter import *
+import random
+
+
+class Foo:
+    def __init__(self, parent):
+        print("hello world")
+
+
+# main routine
+if __name__ == "__main__":
+    root = Tk()
+    root.title("title goes here")
+    something = Foo(root)
+    root.mainloop()
