cd2da981f7c4d7e7aaab6ab1aab124a0966d2025
ummmh <eugenioj2@middleton.school.nz>
2022-03-17 13:48:28 +1300

    Create 01_help_gui_v1.py

---
+++ Temperature Converter Task/temperature_converter_task/01_help_gui_v1.py
@@ -0,0 +1,18 @@
+from tkinter import *
+import random
+
+
+class Converter:
+    def __init__(self, parent):
+        print("hello world")
+
+    # Formatting variables
+    background_colour = "#F5F6FF"
+
+
+# main routine
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Temperature Converter Calculator")
+    something = Converter(root)
+    root.mainloop()
