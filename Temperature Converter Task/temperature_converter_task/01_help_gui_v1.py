2fc472edc844cdcb43e7a9f90a574c0ad4ea1182
ummmh <eugenioj2@middleton.school.nz>
2022-03-17 13:52:11 +1300

    Update 01_help_gui_v1.py

--- Temperature Converter Task/temperature_converter_task/01_help_gui_v1.py
+++ Temperature Converter Task/temperature_converter_task/01_help_gui_v1.py
@@ -6,8 +6,13 @@ class Converter:
     def __init__(self, parent):
         print("hello world")
 
-    # Formatting variables
-    background_colour = "#F5F6FF"
+        # Formatting variables
+        background_colour = "#F5F6FF"
+
+        # Converter Main Screen GUI
+        self.converter_frame = Frame(width=300, height=300,
+                                     bg=background_colour)
+        self.converter_frame.grid()
 
 
 # main routine
