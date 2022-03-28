b1fd53cac94f12d5a5cd025eff084aa18c8bcd66
ummmh <eugenioj2@middleton.school.nz>
2022-03-29 12:39:26 +1300

    Update 07_most_recent_v4.py

--- Temperature Converter Task/temperature_converter_task/07_most_recent_v4.py
+++ Temperature Converter Task/temperature_converter_task/07_most_recent_v4.py
@@ -1,28 +1,41 @@
 """ Get data from user and store it in list,
 then display the most recent three entries nicely
 
-Trial 3 - prints list in reverse order
-(no need for extra code or importing extra libraries)
+Final version based on Trial 3 - adds break loop and checks for empty list
 """
 
 # Set up empty list
 all_calculations = []
 
 # Get five entries of data
-for item in range(0, 5):
+get_item = ""
+while get_item != "zz":
     get_item = input("Enter an item: ")
+
+    if get_item == "zz":
+        break
+
     all_calculations.append(get_item)
 
-all_calculations.reverse()
-
-# show that everything made it to the list
-print()
-print("*** FULL LIST ***")
-print(all_calculations)
-
 print()
 
-print("*** MOST RECENT 3 ***")
-# prints all items starting at end of the list
-for item in range(0, 3):
-    print(all_calculations[len(all_calculations) - item - 1])
+if len(all_calculations) == 0:
+    print("Oops - the list is empty!")
+
+else:
+    # show that everything made it to the list
+    print()
+    print("*** FULL LIST ***")
+    print(all_calculations)
+
+    # prints all items starting at end of the list
+    if len(all_calculations) >= 3:
+        print("*** MOST RECENT 3 ***")
+        for item in range(0, 3):
+            print(all_calculations[len(all_calculations) - item - 1])
+
+    else:
+        print("*** ITEMS FROM NEWEST TO OLDEST ***")
+        for item in all_calculations:
+            print(all_calculations[len(all_calculations)
+                                   - all_calculations.index(item) - 1])
